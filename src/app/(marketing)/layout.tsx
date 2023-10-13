333844b77fe5c771668ae8d502f0b8b347f859fe
Sergei Baranovski <sergei@deriv.com>
2023-10-13 16:34:26 +0300

    feat: add simple navigation

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <div>
-      <h1>DASHBOARD LAYOUT</h1>
+      <h1>MARKETING LAYOUT</h1>
       {children}
     </div>
   );
