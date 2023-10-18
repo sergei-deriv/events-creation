b01031dd093921cbca07efd2dbea9b06cbb25225
Sergei Baranovski <sergei@deriv.com>
2023-10-18 18:56:00 +0300

    feat: fetch the data inside of the server component

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Card = () => {
+  const [state, setState] = useState();
+  //   setDefaultHighWaterMark;
+  return <div>Card</div>;
+};
+
+export default Card;
