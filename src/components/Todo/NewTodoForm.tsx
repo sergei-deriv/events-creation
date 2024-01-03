6e0203caba7ee2fdc7fc1675b36b210bbdcd8819
Sergei Baranovski <sergei@deriv.com>
2024-01-03 12:47:49 +0300

    feat: add new todo to db

--- src/components/Todo/NewTodoForm.tsx
+++ src/components/Todo/NewTodoForm.tsx
@@ -1,9 +1,12 @@
+import { newTodo } from "@/utils/actions";
+
 const NewTodoForm = () => {
   return (
     <div>
       <h2>NewTodoForm</h2>
-      <form>
-        <input type='text' className="border border-black/25"/>
+      <form action={newTodo}>
+        <input name='todoName' type='text' className="border border-black/25"/>
+        <button type="submit">new todo</button>
       </form>
     </div>
   );
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,10 +1,13 @@
 'use server';
+import { revalidatePath } from 'next/cache';
 import db from './db';
 
-export const newTodo = async (content: string) => {
+export const newTodo = async (formData: FormData) => {
   const todo = await db.todo.create({
     data: {
-      content,
+      content: String(formData.get('todoName')),
     },
   });
+
+  revalidatePath('/todos');
 };
